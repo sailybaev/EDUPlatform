869b72321f963c1c8b53366bac89e04254cc92a0
Alikhan <sailybaevvv@gmail.com>
2024-12-19 22:02:40 +0500

    hotfix

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ app.use(cors({
         'http://localhost:5500',
         'http://127.0.0.1:5500',
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        'https://eduplatform-c8bs.onrender.com'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'x-auth-token'],
